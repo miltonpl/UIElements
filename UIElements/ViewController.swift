b8c8f6d20377f696a9cff5a2036286cf42047959
Milton Palaguachi <miltonpalaguachi@gmail.com>
2020-10-09 12:30:53 -0400

    Initial Commit

---
+++ UIElements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIElements
+//
+//  Created by Milton Palaguachi on 10/9/20.
+//  Copyright © 2020 Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
